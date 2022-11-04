24f4e8051b296e723b19a2c118a6f24df7d29a14
nandobfer <nandobfer@yahoo.com.br>
2022-11-04 11:34:10 -0300

    feat: added change coupons

--- routes/bapka/index.js
+++ routes/bapka/index.js
@@ -12,4 +12,7 @@ router.use('/login', login);
 const search_cpf = require('./search_cpf');
 router.use('/search_cpf', search_cpf);
 
+const modificar_cupons = require('./modificar_cupons');
+router.use('/modificar_cupons', modificar_cupons);
+
 module.exports = router;
