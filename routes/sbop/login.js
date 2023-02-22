3f693f3e441b883a5a5bc16170f1d933515650d4
nandobfer <nandobfer@yahoo.com.br>
2023-02-22 09:01:02 -0300

    feat: added pagseguro member fetch

--- routes/sbop/login.js
+++ routes/sbop/login.js
@@ -5,7 +5,7 @@ const newMysql = require('../../src/database')
 
 
 /* GET users listing. */
-router.post('/', function(request, response, next) {    
+router.post('/', (request, response, next) => {    
 	const data = request.body;
 
 	const mysql = newMysql(config.sbop.database);
@@ -21,6 +21,25 @@ router.post('/', function(request, response, next) {
 	}, (error, results) => {
 		if (error) console.error(error);
 		console.log(results);
+		response.json(results);
+		mysql.end();
+	});
+
+
+});
+
+router.post('/pagseguro', (request, response, next) => {    
+	const data = request.body;
+
+	const mysql = newMysql(config.sbop.database);
+	mysql.connect();
+	
+	mysql.query({
+		sql: `SELECT * FROM Membros WHERE id = ? ;`,
+		timeout: 40000, // 40s
+		values: [ data.id ]
+	}, (error, results) => {
+		if (error) console.error(error);
 		response.json(results);
 		mysql.end();
 	});
