3569ee0bef8c05e6c2dd37c06d2bcf33cd3652c3
nandobfer <nandobfer@yahoo.com.br>
2023-03-12 00:11:38 -0300

    feat: added sbop recover password

--- routes/sbop/login.js
+++ routes/sbop/login.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const config = require('../../config.json')
 const newMysql = require('../../src/database')
+const { exec } = require('child_process');
 
 
 router.post('/', (request, response, next) => {    
@@ -11,7 +12,7 @@ router.post('/', (request, response, next) => {
 	mysql.connect();
 	
 	mysql.query({
-		sql: `SELECT * FROM Membros WHERE (user = ? OR cpf = ? OR telefone = ?) AND senha = ?`,
+		sql: `SELECT * FROM Membros WHERE (user = ? OR cpf = ? OR email = ?) AND senha = ?`,
 		timeout: 40000, // 40s
 		values: [
 			data.login,
@@ -31,5 +32,33 @@ router.post('/', (request, response, next) => {
 	});
 });
 
+router.post('/recover', (request, response) => {
+    const data = request.body
+    const mysql = newMysql(config.sbop.database)
+    mysql.connect()
+
+    mysql.query({
+        sql: "SELECT * FROM Membros WHERE USER = ? OR cpf = ? OR email = ?",
+        values: [ data.input, data.input, data.input ]
+    }, (error, results) => {
+		if (error) console.error(error);
+
+        const member = results[0]
+        console.log(member)
+        
+        if (member) {
+            exec(`python src/sbop/recover_password.py ${member.user} ${member.email}`, (err, stdout, stderr) => {
+                if (err) console.error(err)
+                if (stderr) console.error(stderr)
+                
+                console.log(stdout)
+                response.json(member)
+            })
+        } else {
+            response.json({error: 'nothing found'})
+        }
+    })
+
+})
 
 module.exports = router;
