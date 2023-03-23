bed156445801915b05f6306e380c2ff624e0156b
nandobfer <nandobfer@yahoo.com.br>
2023-03-23 12:41:54 -0300

    feat: added multer middleware

--- app.js
+++ app.js
@@ -2,8 +2,21 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+const multer = require('multer');
 
 var app = express();
+
+// Set up multer storage configuration
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, 'documents/'); // Set the destination folder for uploaded files
+    },
+    filename: (req, file, cb) => {
+      cb(null, `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`); // Set a unique filename for the uploaded file
+    },
+});
+
+const upload = multer({ storage: storage });
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
