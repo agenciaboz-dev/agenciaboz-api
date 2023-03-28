4c87d8d437afd9aa6da421561eecdbcdbd4be310
nandobfer <nandobfer@yahoo.com.br>
2023-03-28 18:02:38 -0300

    update: generate contract and upload scripts

--- routes/sion/contract.js
+++ routes/sion/contract.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const config = require('../../config.json')
 const newMysql = require('../../src/database')
-const { execSync } = require('child_process')
+const { execSync, exec } = require('child_process')
 const fs = require("fs");
 const path = require("path");
 
@@ -49,7 +49,7 @@ router.post('/lead', (request, response, next) => {
     mysql.connect();
     
     mysql.query({
-        sql: "INSERT INTO contracts (unit, date, pessoa, supplier, name, email, phone, address, cep, cnpj, company, category, cpf, rg) VALUES (?)",
+        sql: "INSERT INTO contracts (unit, date, pessoa, supplier, name, email, phone, address, cep, cnpj, company, category, cpf, rg, seller, seller_name) VALUES (?)",
         values: [[
             data.unit,
             data.date,
@@ -65,6 +65,8 @@ router.post('/lead', (request, response, next) => {
             data.category,
             data.cpf?.replaceAll('.', '').replaceAll('-', ''),
             data.rg?.replaceAll('.', '').replaceAll('-', ''),
+            data.seller.id,
+            data.seller.name,
         ]]
     }, (error, results) => {
         if (error) {
@@ -84,7 +86,7 @@ router.post('/', (request, response, next) => {
     
 })
 
-router.post('/new', (request, response, next) => {    
+router.post('/generate', (request, response, next) => { 
 	const data = JSON.parse(request.body.data);
     data.date = new Date()
     
@@ -94,61 +96,49 @@ router.post('/new', (request, response, next) => {
 	mysql.connect();
 
     mysql.query({
-        sql: "INSERT INTO contracts (unit, date, pessoa, supplier, name, email, phone, address, cep, cnpj, company, category, cpf, rg) VALUES (?)",
-        values: [[
-            data.unit,
-            data.date,
-            data.pessoa,
-            data.supplier,
-            data.name,
-            data.email,
-            data.phone.replaceAll('(', '').replaceAll('-', '').replaceAll(')', '').replaceAll(' ', ''),
-            data.address,
-            data.cep.replaceAll('.', '').replaceAll('-', ''),
-            data.cnpj?.replaceAll('.', '').replaceAll('-', '').replaceAll('/', ''),
-            data.company,
-            data.category,
-            data.cpf?.replaceAll('.', '').replaceAll('-', ''),
-            data.rg?.replaceAll('.', '').replaceAll('-', ''),
-        ]]
+        sql: "SELECT * FROM contracts where unit = ?",
+        values: [ data.unit ]
     }, (error, results) => {
-        if (error) {
-            console.error(error)
-            response.json({error: error.sqlMessage.includes('unit') ? 'Unidade consumidora já cadastrada': 'Erro desconhecido na API'})
-        } else {
 
-            data.id = results.insertId
-            data.date = data.date.toLocaleDateString('pt-BR')
-            response.json(results)
+        const contract = results[0]
 
-            // Create an 'uploads' folder if it doesn't exist
-            const uploadsDir = `documents/sion/${data.id}`
-            if (!fs.existsSync(uploadsDir)) {
-                fs.mkdirSync(uploadsDir, { recursive: true })
-            }
+        contract.date = contract.date.toLocaleDateString('pt-BR')
 
-            // Iterate through the files and save them
-            Object.entries(files).forEach(([key, file]) => {
-                const filePath = path.join(uploadsDir, file.name);
-                console.log(filePath)
-                file.mv(filePath, (err) => {
-                    if (err) {
-                        console.error("Error saving file:", err);
-                        return res.status(500).json({ error: "Error saving file" });
-                    }
-                })
-            });
-
-            const input = JSON.stringify(data).replaceAll('"', "'")
-            const command = `python3 src/sion/contract.py "${input}"`
-            setTimeout(() => {
-                console.log(command)
-                const output = execSync(command)
-                console.log(output.toString())
-            }, 1000 * 10)
-            // execSync(`chown agenciaboz:agenciaboz /home/agenciaboz/dev.agenciaboz.com.br/static/documents/${member.id}/certificate.pdf`)
-    
+        // Create an 'uploads' folder if it doesn't exist
+        const uploadsDir = `documents/sion/${contract.id}`
+        if (!fs.existsSync(uploadsDir)) {
+            fs.mkdirSync(uploadsDir, { recursive: true })
         }
+
+        // Iterate through the files and save them
+        Object.entries(files).forEach(([key, file]) => {
+            const filePath = path.join(uploadsDir, file.name);
+            console.log(filePath)
+            file.mv(filePath, (err) => {
+                if (err) {
+                    console.error("Error saving file:", err);
+                    return res.status(500).json({ error: "Error saving file" });
+                }
+            })
+        });
+
+        console.log(contract)
+
+        const input = JSON.stringify(contract).replaceAll('"', "'")
+        const generate_contract = `python3 src/sion/contract.py "${input}"`
+        exec(generate_contract, (error, stdout, stderr) => {
+            console.log(stdout)
+            
+            // response.setHeader('Content-Type', 'application/pdf');
+            response.sendFile(`sion/${contract.id}/contract.pdf`, { root: path.join('documents') });
+
+            exec(`python3 src/sion/upload.py "${input}"`, (error, stdout, stderr) => {
+                console.log(stdout)
+            })
+        })
+
+        // execSync(`chown agenciaboz:agenciaboz /home/agenciaboz/dev.agenciaboz.com.br/static/documents/${member.id}/certificate.pdf`)
+    
 
     })
 	
--- src/sion/upload.py
+++ src/sion/upload.py
@@ -1,5 +1,22 @@
-root = '1wL5SVt2zJdqFgEd20SdYajdaPI-eTygu'
+import sys, json, os
+from pathlib import Path
+sys.path.append(str(Path("src/gdrive").resolve()))
+from gdrive import upload, mkdir
+
+cadastros = '1wL5SVt2zJdqFgEd20SdYajdaPI-eTygu'
+
+data = sys.argv[1]
+data = data.replace("'", '"')
+data = json.loads(data)
+
+path = f"documents/sion/{data['id']}"
+if not os.path.exists(path):
+    os.makedirs(path)
+
+seller = data['seller_name']
+seller_folder = mkdir(cadastros, seller)
 
 files = [file for file in Path(path).glob("*") if file.is_file()]
 for file in files:
-    upload(data['id'], os.path.join(Path(path), file.name))
+    print(os.path.join(Path(path), file.name))
+    upload(seller_folder['id'], data['unit'], os.path.join(Path(path), file.name))
