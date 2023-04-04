e6bc1fcb48247e5e0c337965b5dad5e7bd4f9a70
nandobfer <nandobfer@yahoo.com.br>
2023-04-04 14:20:09 -0300

    db: rdstation id not auto increment

---
+++ prisma/migrations/20230404171757_rdstation_id/migration.sql
@@ -0,0 +1,14 @@
+-- DropIndex
+DROP INDEX `contracts_seller_id_fkey` ON `contracts`;
+
+-- AlterTable
+ALTER TABLE `rdstation` MODIFY `id` INTEGER NOT NULL;
+
+-- AddForeignKey
+ALTER TABLE `contracts` ADD CONSTRAINT `contracts_seller_id_fkey` FOREIGN KEY (`seller_id`) REFERENCES `users`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `financial` ADD CONSTRAINT `financial_contract_id_fkey` FOREIGN KEY (`contract_id`) REFERENCES `contracts`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `rdstation` ADD CONSTRAINT `rdstation_contract_id_fkey` FOREIGN KEY (`contract_id`) REFERENCES `contracts`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
