daee5542b11e45dcdd99d2677e25fb2685568890
nandobfer <nandobfer@yahoo.com.br>
2023-04-04 09:59:42 -0300

    fix: typo

--- src/sion/rdstation.js
+++ src/sion/rdstation.js
@@ -10,7 +10,7 @@ const token = '?token=6422e7534495ab000b1b42cd'
 api.token = token
 
 api.lead = (data) => {
-    api.post('/deals'+rdstation.token, data)
+    api.post('/deals'+api.token, data)
     .then(response => {
         console.log(response.data)
         return response.data
