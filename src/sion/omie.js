1c13d4c90837cca3dce839155a0943228f98e2d1
nandobfer <nandobfer@yahoo.com.br>
2023-04-11 11:55:53 -0300

    feat: added omie api file

---
+++ src/sion/omie.js
@@ -0,0 +1,31 @@
+const axios = require('axios');
+const { PrismaClient } = require('@prisma/client')
+
+const prisma = new PrismaClient()
+
+const api = axios.create({
+    baseURL: 'https://app.omie.com.br/api/v1',
+    timeout: 1000 * 10,
+})
+
+api.signup = ( contract ) => {
+    const data = {
+        call: "IncluirCliente",
+        app_key: KEY,
+        app_secret: SECRET,
+        param: [
+            {
+                codigo_cliente_integracao: contract.id.toString(),
+                email: contract.email,
+                razao_social: contract.company || contract.name,
+                nome_fantasia: contract.name
+            }
+        ]
+    }
+    api.post('/geral/clientes/', data)
+    .then(response => console.log(response.data))
+    .catch(error => console.error(error))
+}
+
+const KEY = '2937495729168'
+const SECRET = 'd8bc84dfc0e30d30a188f70a5ddf5550'
