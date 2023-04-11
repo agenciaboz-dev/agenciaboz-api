d38989ced78b07efd165626e6d4ea024244162be
nandobfer <nandobfer@yahoo.com.br>
2023-04-11 16:48:43 -0300

    test: omie

--- src/sion/omie.js
+++ src/sion/omie.js
@@ -26,6 +26,7 @@ api.signup = ( contract ) => {
     api.post('/geral/clientes/', data)
     .then(async (response) => {
         const id = response.data.codigo_cliente_omie
+        console.log({omie_id: id})
         await prisma.omie.create({ data: { id, contract_id: contract.id } })
     })
     .catch(error => console.error(error))
