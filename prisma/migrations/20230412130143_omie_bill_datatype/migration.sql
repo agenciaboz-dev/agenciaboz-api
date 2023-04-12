73c4a5f3f4beec3f433a16fda3c4c8f339229143
nandobfer <nandobfer@yahoo.com.br>
2023-04-12 10:01:58 -0300

    db: omie bill datatype

---
+++ prisma/migrations/20230412130143_omie_bill_datatype/migration.sql
@@ -0,0 +1,35 @@
+/*
+  Warnings:
+
+  - You are about to alter the column `bill` on the `omie` table. The data in that column could be lost. The data in that column will be cast from `VarChar(191)` to `Int`.
+
+*/
+-- DropIndex
+DROP INDEX `contracts_seller_id_fkey` ON `contracts`;
+
+-- DropIndex
+DROP INDEX `logs_contract_id_fkey` ON `logs`;
+
+-- DropIndex
+DROP INDEX `logs_seller_id_fkey` ON `logs`;
+
+-- AlterTable
+ALTER TABLE `omie` MODIFY `bill` INTEGER NULL;
+
+-- AddForeignKey
+ALTER TABLE `contracts` ADD CONSTRAINT `contracts_seller_id_fkey` FOREIGN KEY (`seller_id`) REFERENCES `users`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `financial` ADD CONSTRAINT `financial_contract_id_fkey` FOREIGN KEY (`contract_id`) REFERENCES `contracts`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `rdstation` ADD CONSTRAINT `rdstation_contract_id_fkey` FOREIGN KEY (`contract_id`) REFERENCES `contracts`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `omie` ADD CONSTRAINT `omie_contract_id_fkey` FOREIGN KEY (`contract_id`) REFERENCES `contracts`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `logs` ADD CONSTRAINT `logs_contract_id_fkey` FOREIGN KEY (`contract_id`) REFERENCES `contracts`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `logs` ADD CONSTRAINT `logs_seller_id_fkey` FOREIGN KEY (`seller_id`) REFERENCES `users`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
