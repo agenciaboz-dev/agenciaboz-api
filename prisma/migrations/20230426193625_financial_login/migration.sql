c075b141b38ad315ffe53b4aef273397f684e826
nandobfer <nandobfer@yahoo.com.br>
2023-04-26 16:37:09 -0300

    db: added login and password to financial

---
+++ prisma/migrations/20230426193625_financial_login/migration.sql
@@ -0,0 +1,30 @@
+-- DropIndex
+DROP INDEX `contracts_seller_id_fkey` ON `contracts`;
+
+-- DropIndex
+DROP INDEX `logs_contract_id_fkey` ON `logs`;
+
+-- DropIndex
+DROP INDEX `logs_seller_id_fkey` ON `logs`;
+
+-- AlterTable
+ALTER TABLE `financial` ADD COLUMN `login` VARCHAR(191) NULL,
+    ADD COLUMN `password` VARCHAR(191) NULL;
+
+-- AddForeignKey
+ALTER TABLE `contracts` ADD CONSTRAINT `contracts_seller_id_fkey` FOREIGN KEY (`seller_id`) REFERENCES `users`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `financial` ADD CONSTRAINT `financial_contract_id_fkey` FOREIGN KEY (`contract_id`) REFERENCES `contracts`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `rdstation` ADD CONSTRAINT `rdstation_contract_id_fkey` FOREIGN KEY (`contract_id`) REFERENCES `contracts`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `omie` ADD CONSTRAINT `omie_contract_id_fkey` FOREIGN KEY (`contract_id`) REFERENCES `contracts`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `logs` ADD CONSTRAINT `logs_contract_id_fkey` FOREIGN KEY (`contract_id`) REFERENCES `contracts`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `logs` ADD CONSTRAINT `logs_seller_id_fkey` FOREIGN KEY (`seller_id`) REFERENCES `users`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
